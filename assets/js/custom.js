aefbece3e4f1aade6afe576abbbfeab579f0046c
mrpetef10 <pete.mardell@f10.co.uk>
2018-04-29 13:47:45 +0100

    Completed form validation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -29,6 +29,101 @@ $(document).ready(function(){
 		$('.hide-join-form').slideToggle("medium");
 	});
 	
+	
+	// Validate email input
+	$('input[name="email"]').focusout(function(){
+		validateEmailField();
+	});
+	
+	
+	// Check passwords entered on signup forms are valid
+	$('input[name="password"]').focusout(function(){
+		validatePasswordField();
+	});
+	
+	
+	// Check passwords match
+	$('input[name="passwordCheck"]').focusout(function(){
+		// If passwords match, now check if the original password is correct
+		if(validatePasswordsMatch()){
+			validatePasswordField();
+		}
+	});
+
+	
+	function validateEmailField(){
+		// Validate the email
+		var email = $('input[name="email"]').val();
+		
+		if(!validateEmail(email)){
+			$('label[data-label="email"]').parent().removeClass("success").addClass("error");
+			$('label[data-label="email"] .fa-times').show();
+			$('label[data-label="email"] .fa-check').hide();
+			return false;
+		}else{
+			$('label[data-label="email"]').parent().removeClass("error").addClass("success");
+			$('label[data-label="email"] .fa-times').hide();
+			$('label[data-label="email"] .fa-check').show();
+			return true;
+		}
+	}
+	
+	
+	function validatePasswordField(){
+		// Validate the password
+		// Must be greater than 6 characters, contain one capital and one digit
+		var password = $('input[name="password"]').val();
+		
+		if(!validatePassword(password)){
+			$('label[data-label="password"]').parent().removeClass("success").addClass("error");
+			$('label[data-label="password"] .fa-times').show();
+			$('label[data-label="password"] .fa-check').hide();
+			return false;
+		}else{
+			$('label[data-label="password"]').parent().removeClass("error").addClass("success");
+			$('label[data-label="password"] .fa-times').hide();
+			$('label[data-label="password"] .fa-check').show();
+			return true;
+		}
+	}
+	
+	function validatePasswordsMatch(){
+		var password = $('input[name="password"]').val();
+		var passwordCheck = $('input[name="passwordCheck"]').val();
+		
+		if(password != passwordCheck){
+			$('label[data-label="password"], label[data-label="passwordCheck"]').parent().removeClass("success").addClass("error");
+			$('label[data-label="passwordCheck"] .fa-times').show();
+			$('label[data-label="passwordCheck"] .fa-check').hide();
+			return false;
+		}else{
+			$('label[data-label="passwordCheck"]').parent().removeClass("error").addClass("success");
+			$('label[data-label="passwordCheck"] .fa-times').hide();
+			$('label[data-label="passwordCheck"] .fa-check').show();
+			return true;
+		}
+	}
+	
+	function validatePassword(str) {
+		if (str.length < 7) {
+			return false;
+		} else if (str.length > 99) {
+			return false;
+		} else if (str.search(/\d/) == -1) {
+			return false;
+		} else if (str.search(/[a-z]/) == -1) {
+			return false;
+		} else if (str.search(/[A-Z]/) == -1) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+	function validateEmail(email) {
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(String(email).toLowerCase());
+	}
 
 	/** SOCKET REQUESTS */
 	// A new player joined
@@ -53,7 +148,7 @@ $(document).ready(function(){
 		playerJoinedMsgCount < 2){
 			$.jnoty("A new player just joined the party!", {
 				header: 'New Player',
-				sticky: true,
+				sticky: false,
 				theme: 'jnoty-success',
 				icon: 'fa fa-check-circle',
 				life: 2000
